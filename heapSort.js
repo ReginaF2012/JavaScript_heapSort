471039bd21d1fcb27b39affe4a5534a7f5961d70
ReginaF2012 <skritschy@gmail.com>
2021-01-24 12:11:10 -0600

    heapify function

--- heapSort.js
+++ heapSort.js
@@ -1,3 +1,23 @@
 function swap(array, index1, index2) {
     [array[index1], array[index2]] = [array[index2], array[index1]];
 }
+
+function heapify(array, index = 0, length = array.length) {
+    let largest = index,
+        left = index * 2 + 1,
+        right = index * 2 + 2;
+
+    if (left < length && array[left] > array[largest]) {
+        largest = left;
+    }
+    if (right < length && array[right] > array[largest]) {
+        largest = right;
+    }
+
+    if (largest !== index) {
+        swap(array, index, largest);
+        heapify(array, largest);
+    }
+
+    return array;
+}
